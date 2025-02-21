56cc66e88af9a1cf0abaffd425b06be68ad25cf6
AngyUnU <23610118@utgz.edu.mx>
2025-02-21 13:10:17 -0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('develop', function(){
+    return 'Welcome to Developments';
+})->name('develope.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('develop/{develops}',function($develops){
+    if($develops === '5'){
+        return redirect()-> route('develope.index');
+    }
+   return 'Detalles del desarrollador '. $develops;
+});
+
+
+//Route::get('/dashboard', function () {
+  //return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+  Route::view('/dashboard','dashboard')->name('dashboard');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+}); 
+
+Route::get('/post', function(){
+return view ('post');
+})->name('post.index');
 
 require __DIR__.'/auth.php';
